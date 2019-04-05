c6f30b25e0d23a89993345cbe2e88b50534bdcfa
Matteo Beretta <matteo@trendupgraphic.com>
2019-04-05 12:17:09 +0200

    Base

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# pixelAspectRatio4Zeplin
-A bookmarklet to help working on Zeplin for displays with non square pixel aspect ratio.
+# Pixel aspect ratio helper for Zeplin
+## A bookmarklet to help working on Zeplin for displays with non square pixel aspect ratio.
+
+[Zeplin](http://zeplin.io) helps the collaborations between designers and developers but – at now – it doesn’t support non square [pixel aspect ratio (PAR)](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=16&cad=rja&uact=8&ved=2ahUKEwju2evk2bjhAhUWwAIHHY-XAzgQFjAPegQIBBAB&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FPixel_aspect_ratio&usg=AOvVaw2uBdzUfwVSXke0BpvuInKs) used often on industrial screens.
+If you need to read horizontal measures distorted by a specific pixel aspect ratio you can use this [bookmarklet](https://en.wikipedia.org/wiki/Bookmarklet).
+
+![size](https://github.com/teoteo/pixelAspectRatio4Zeplin/size.gif)
+![distances](https://github.com/teoteo/pixelAspectRatio4Zeplin/distance.gif)
+
+##How to:
+
+1. Identify the pixel pitch of destination display;
+2. copy this piece of JavaScript in a text editor (this is ready for a pixel aspect ratio of 0.93) and set the variables pixePithH and pixePithV
+```javascript
+javascript:(function(){
+	pixePithH=0.1926;
+	pixePithV=0.1790;
+	document.body.appendChild(document.createElement('script')).src='https://raw.githubusercontent.com/teoteo/pixelAspectRatio4Zeplin/master/zeplinpar.js';
+})();
+```
+3. add the bookmarklet creating a bookmar using the modified JavaScript as address;
+4. open your Zeplin screen or component and click on the bookmarklet to activare it.
+
+Thanks to @andretti1977 for his help
---
+++ zeplinpar.js
@@ -0,0 +1,55 @@
+function zeplinObserve() {
+/*
+	var pixePithH = 0.1926;
+	var pixePithV = 0.1790;
+*/
+	var pixelAspectRatio = pixePithV/pixePithH;
+    var targetNode = document.querySelector('main');
+
+// Options for the observer (which mutations to observe)
+var config = { attributes: true, childList: true, subtree: true };
+
+
+// Callback function to execute when mutations are observed
+var callback = function(mutationsList, observer) {
+    for(var mutation of mutationsList) {
+        if (mutation.type == 'childList' && mutation.addedNodes.length > 0) {
+
+            //console.log('A child node has been added or removed.');
+//console.log(mutation.addedNodes[0]);
+let added = mutation.addedNodes[0];
+if (added.className == 'layerWidth') {
+
+           let ww = '<div style="white-space: nowrap; margin-bottom: 10px;"><span style="font-weight: 500;">' + Math.round((parseInt(added.innerHTML.replace('px',''), 10) * pixelAspectRatio)) + '</span>px' + '<span style="font-size: 10px; color: #888;">('+added.innerHTML+')</span></div>';
+//     console.log('pixelAspectRatio: ' + pixelAspectRatio);
+
+           added.innerHTML = ww;
+} else if (added.className=='distances'){
+    //console.log(added.className);
+    
+    added.querySelectorAll('.distance.horizontal').forEach(function(disOrEl){
+    disOrEl.querySelectorAll('.value').forEach(function(valEl){
+
+    let newWW = '<span style="font-weight: 500;">' + Math.round((parseInt(valEl.innerHTML.replace('px',''), 10) * pixelAspectRatio)) + '</span>px<span style="font-size: 10px; color: #888;">' + '('+valEl.innerHTML+')</span>';
+
+    valEl.innerHTML = newWW;
+    });
+    });
+    
+    
+}
+        }
+        else if (mutation.type == 'attributes') {
+            //console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+}
+
+zeplinObserve();
